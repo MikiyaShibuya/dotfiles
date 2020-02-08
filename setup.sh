81f51a1e8abb40c3dd904559528f4e5aac6fa742
MikiyaShibuya <mikiya.mail4univ@gmail.com>
2020-02-08 13:59:09 +0900

    Add zsh to repo

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 ln -s $HOME/dotfiles/_vimrc_ritch $HOME/.vimrc
 ln -s $HOME/dotfiles/_gitconfig $HOME/.gitconfig
 ln -s $HOME/dotfiles/_tmux.conf $HOME/.tmux.conf
+ln -s $HOME/dotfiles/zshrc $HOME/.zshrc
+ln -s $HOME/dotfiles/oh-my-zsh $HOME/.oh-my-zsh
