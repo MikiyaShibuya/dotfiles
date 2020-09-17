730f4b6844118dc042581bc6a6c1926f26f04a13
MikiyaShibuya <mikiya.mail4univ@gmail.com>
2020-09-17 23:33:20 +0900

    Fix format

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
-ln -s $HOME/dotfiles/_vimrc_ritch $HOME/.vimrc
+# config for vim
+ln -s $HOME/dotfiles/_vimrc $HOME/.vimrc
+
 ln -s $HOME/dotfiles/_gitconfig $HOME/.gitconfig
 ln -s $HOME/dotfiles/_tmux.conf $HOME/.tmux.conf
