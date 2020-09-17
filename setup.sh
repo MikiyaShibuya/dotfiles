ce6b1d68efbc51eef63bfd33f02a737c07de2a0a
MikiyaShibuya <mikiya.mail4univ@gmail.com>
2020-09-17 23:51:25 +0900

    Update for NeoVim

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 # config for vim
 ln -s $HOME/dotfiles/_vimrc $HOME/.vimrc
 
+# config for nvim
+mkdir -p  $HOME/.config/nvim
+ln -s $HOME/dotfiles/_init.vim $HOME/.config/nvim/init.vim
+
 ln -s $HOME/dotfiles/_gitconfig $HOME/.gitconfig
 ln -s $HOME/dotfiles/_tmux.conf $HOME/.tmux.conf
