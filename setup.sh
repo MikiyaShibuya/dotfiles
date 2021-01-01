ed7ffed47678a7ce53eb22a5745e9efed71d35de
MikiyaShibuya <mikiya.mail4univ@gmail.com>
2021-01-01 17:32:51 +0900

    Add p10k.zsh

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ ln -s $HOME/dotfiles/_init.vim $HOME/.config/nvim/init.vim
 ln -s $HOME/dotfiles/_gitconfig $HOME/.gitconfig
 ln -s $HOME/dotfiles/_tmux.conf $HOME/.tmux.conf
 ln -s $HOME/dotfiles/zshrc $HOME/.zshrc
+ln -s $HOME/dotfiles/p10k.zsh $HOME/.p10k.zsh
 
 ln -s $HOME/dotfiles/dein.toml $HOME/.config/nvim/dein.toml
 ln -s $HOME/dotfiles/dein_lazy.toml $HOME/.config/nvim/dein_lazy.toml
