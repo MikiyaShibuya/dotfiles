94014e6fe25607cc71ee60881efcd60996bbf6c8
shibuya <shibuya@Shibuyas-MacBook-Air.local>
2021-05-13 16:06:54 +0900

    Update file name and setup script

--- setup.sh
+++ setup.sh
@@ -5,12 +5,14 @@ ln -nfs $PWD/vimrc $HOME/.vimrc
 mkdir -p  $HOME/.config/nvim
 ln -nfs $PWD/init.vim $HOME/.config/nvim/init.vim
 
+# git, tmux, zshrc setting
 ln -nfs $PWD/gitconfig $HOME/.gitconfig
 ln -nfs $PWD/tmux.conf $HOME/.tmux.conf
-ln -s $PWD/zshrc $HOME/.zshrc
-ln -s $PWD/p10k.zsh $HOME/.p10k.zsh
+ln -nfs $PWD/zshrc $HOME/.zshrc
+ln -nfs $PWD/p10k.zsh $HOME/.p10k.zsh
 
-ln -s $PWD/dein.toml $HOME/.config/nvim/dein.toml
-ln -s $PWD/dein_lazy.toml $HOME/.config/nvim/dein_lazy.toml
-ln -s $PWD/coc-settings.json $HOME/.config/nvim/coc-settings.json
+# dein & coc settings for nvim
+ln -nfs $PWD/dein.toml $HOME/.config/nvim/dein.toml
+ln -nfs $PWD/dein_lazy.toml $HOME/.config/nvim/dein_lazy.toml
+ln -nfs $PWD/coc-settings.json $HOME/.config/nvim/coc-settings.json
 
