e4eec34c47345e2d4788a75e4de5d0ac5430ae40
shibuya <shibuya@Shibuyas-MacBook-Air.local>
2021-05-13 15:44:42 +0900

    Update file name

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 # config for vim
-ln -s $HOME/dotfiles/_vimrc $HOME/.vimrc
+ln -s $PWD/vimrc $HOME/.vimrc
 
 # config for nvim
 mkdir -p  $HOME/.config/nvim
-ln -s $HOME/dotfiles/_init.vim $HOME/.config/nvim/init.vim
+ln -s $PWD/init.vim $HOME/.config/nvim/init.vim
 
-ln -s $HOME/dotfiles/_gitconfig $HOME/.gitconfig
-ln -s $HOME/dotfiles/_tmux.conf $HOME/.tmux.conf
+ln -s $PWD/gitconfig $HOME/.gitconfig
+ln -s $PWD/tmux.conf $HOME/.tmux.conf
