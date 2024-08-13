35e21ee0a32d66a32527ba0ac3afd38a759da340
MikiyaShibuya <mikiya.mail4univ@gmail.com>
2024-08-13 23:07:51 +0900

    Implement fast cursor

--- nvim/init.lua
+++ nvim/init.lua
@@ -38,6 +38,43 @@ opt.ignorecase = true
 opt.mouse = "a"
 
 
+local keymap = vim.keymap
+local default_opts = { silent = true, noremap = true }
+
+-- Move cursor faster
+vim.g.base_time = 0
+vim.g.last_time = 0
+vim.g.last_key = ""
+local function fast_cursor(key, n_repeat)
+  return function()
+    -- get current time
+    local current_time = vim.loop.hrtime() / 1e6
+
+
+    if current_time > vim.g.last_time + 100 then
+      vim.g.base_time = current_time
+    end
+
+    if vim.g.last_key ~= key then
+      vim.g.base_time = current_time
+    end
+
+    vim.g.last_time = current_time
+    vim.g.last_key = key
+
+    if vim.g.last_time > vim.g.base_time + 100 then
+      vim.cmd('norm! ' .. key:rep(n_repeat))
+    else
+      vim.cmd('norm! ' .. key)
+    end
+  end
+end
+vim.keymap.set("n", "k", fast_cursor("k", 3), default_opts)
+vim.keymap.set("n", "j", fast_cursor("j", 3), default_opts)
+vim.keymap.set("n", "h", fast_cursor("h", 3), default_opts)
+vim.keymap.set("n", "l", fast_cursor("l", 3), default_opts)
+
+
 require("lazy").setup({
   "williamboman/mason.nvim",
   "williamboman/mason-lspconfig.nvim",
