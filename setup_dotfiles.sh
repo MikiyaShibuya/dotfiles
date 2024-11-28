ef8be1e0188dd6571ae8b801315a9dd0bf156e6e
MikiyaShibuya <mikiya.mail4univ@gmail.com>
2024-11-28 11:14:32 +0900

    Fix setup command failure

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-# curl -fsL https://raw.githubusercontent.com/MikiyaShibuya/dotfiles/refs/heads/master/setup_dotfiles.sh | sudo bash -s --
+# curl -fsL https://raw.githubusercontent.com/MikiyaShibuya/dotfiles/refs/heads/master/setup_dotfiles.sh | bash -s --
 
 set -e
 
-export USER=$USER
-export HOMEDIR=$(echo ~$USER)
+export USER=$(whoami)
+export HOMEDIR=$(eval echo ~$USER)
 REPO_URL=https://github.com/MikiyaShibuya/dotfiles.git
 
 mkdir -p $HOMEDIR/.local/share
 cd $HOMEDIR/.local/share
+
+if [ -d dotfiles ]; then
+  echo "Uninstalling previous installed dotfiles"
+  cd dotfiles
+  if [ -f uninstall.sh ]; then
+    ./uninstall.sh | true
+  fi
+  cd ..
+  rm -r dotfiles
+fi
 
 git clone $REPO_URL --depth=1
 cd dotfiles
