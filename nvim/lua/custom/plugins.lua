92616f46c56d51efb1b65bb1b4e440553514e4f5
mshibuya-pfr <mshibuya@pfrobotics.jp>
2026-01-20 13:44:28 +0900

    Unset markdown preview port

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -39,7 +39,6 @@ local plugins = {
       vim.g.mkdp_echo_preview_url = 1
       vim.g.mkdp_open_to_the_world = 1
       vim.g.mkdp_open_ip = '0.0.0.0'
-      vim.g.mkdp_port = 8888
     end,
   },
 }
